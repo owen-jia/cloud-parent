43c6b0a198e0e529982c9b3f17efc01a616e0641
贾文军 <jiawenjun@isesol.com>
2018-07-23 18:02:30 +0800

    使用本地文件系统进行配置文件管理

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# cloud-parent
-spring-cloud-base-project
+## cloud parent 微服务框架
+
+> Author: Owen Jia    
+> Email:  owen-jia@outlook.com
+
+> 服务依赖spring cloud 1.5.6.RELEASE(Dalston.SR4)
+
+> 项目依赖spring boot & maven搭建
+
+> 部署运行方式 java -jar xxx.jar
+
+### 子项目介绍
+
+- cloud-service-discover 注册中心服务
+- cloud-service-gateway 网关服务
+- cloud-service-config 配置中心服务
+- cloud-service-demo1 消费者服务
+- cloud-service-demo2 生产者服务
+
+
+### cloud-service-discover
+
+### cloud-service-gateway
+
+### cloud-service-config
+
+### cloud-service-demo1
+
+### cloud-service-demo2
+
+
+
--- cloud-service-config/src/main/java/com/ts/config/ConfigServerApp.java
+++ cloud-service-config/src/main/java/com/ts/config/ConfigServerApp.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 配置服务启动器
@@ -14,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @SpringBootApplication
 @EnableConfigServer
-@ComponentScan("com.ts.config.*")
+@ComponentScan(basePackages = "com.ts.config")
 public class ConfigServerApp {
 
     public static void main(String[] args) {
