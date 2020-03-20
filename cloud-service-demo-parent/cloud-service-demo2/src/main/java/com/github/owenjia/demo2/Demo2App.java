78ac39203b4fd79b4f86cf254f6acdb19bb1a813
贾文军 <jiawenjun@isesol.com>
2020-03-20 18:11:15 +0800

    add openfeign module

--- cloud-service-demo-parent/cloud-service-demo2/src/main/java/com/github/owenjia/demo2/Demo2App.java
+++ cloud-service-demo-parent/cloud-service-demo2/src/main/java/com/github/owenjia/demo2/Demo2App.java
@@ -3,12 +3,14 @@ package com.github.owenjia.demo2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Demo2
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class Demo2App {
     public static void main( String[] args ) {
         SpringApplication.run(Demo2App.class, args);
